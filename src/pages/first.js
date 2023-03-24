1b5038370c1e4f0beed16cc86a676b7f4bf3339d
okoyemichael <miichael.okoye.4@gmail.com>
2023-03-24 12:52:35 +0100

    first commit

---
+++ src/pages/first.js
@@ -0,0 +1,199 @@
+import React from 'react'
+import '../styles.css/allstyles.css'
+
+const First = () => {
+  return (
+        <main>
+            <>
+            <div className='navbar-container'>
+                    <div className="navbar">
+                    <div className="logo-img"></div>
+                    <nav>
+                        <ul className='links'>
+                            <li className='nav-link'>Home</li>
+                            <li className='nav-link'>Landlord</li>
+                            <li className='nav-link'>Tenants</li>
+                            <li className='nav-link'>Contact Us</li>
+                        </ul>
+                    </nav>
+                    <hr />
+                    </div>
+                </div>
+                <div className='body-container'>
+                        <h1>The most affortable place to stay in the san franciso bay area</h1>
+                        <div></div>
+                        
+                </div>
+                <div className='image'></div>
+                
+                
+            </>
+            <div className="seconddiv">
+                <div className="secondbody">
+                    <h2>minimum living cost takes care of everything</h2>
+                    <div className="flexcontainer">
+                    <div className="img1"></div>
+                    <div className="griddiv">
+                    <div className="iconcontainer">
+                        <div className="icon1"></div>
+                        <p>Pay as Little as possible!</p>
+                    </div>
+
+                    <div className="iconcontainer">
+                        <div className="icon2"></div>
+                        <p>Enjoy wisdom of community!</p>
+                    </div>
+
+                    <div className="iconcontainer">
+                        <div className="icon3"></div>
+                        <p>Let's somebody else take care of Landlord!</p>
+                    </div>
+
+                    <div className="iconcontainer">
+                        <div className="icon4"></div>
+                        <p>Enjoy peaceful Environment!</p>
+                    </div>
+
+                    <div className="iconcontainer">
+                        <div className="icon5"></div>
+                        <p>Stay Safe! Save Money!</p>
+                    </div>
+
+                    <div className="iconcontainer">
+                        <div className="icon6"></div>
+                        <p>Pay for what you use !</p>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div className="thirddiv">
+                <div className="thirdbody">
+                <div className="properties">
+                <h2>list of properties</h2>
+                <button className='view'>view all properties</button>
+                </div>
+
+                <div className='gridproperties'>
+                    <div className="imagecontainer1">
+                        <div className="imgs1"></div>
+                        <div className="text">
+                            <h4>2578 Folsom street, san francisco, CA, 94110</h4>
+                            <p>Private Room</p>
+                            <h3>$1200/month</h3>
+                        </div>
+                        <div className="widgets">
+                            <p className='p1'>
+                                <div className="tiny1"></div>
+                            </p>
+                            <p className='p2'>
+                                <div className="tiny2"></div>
+                            </p>
+                            <p className='p3'>
+                                <div className="tiny3"></div>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="imagecontainer2">
+                        <div className="imgs2"></div>
+                        <div className="text">
+                            <h4>2578 Folsom street, san francisco, CA, 94110</h4>
+                            <p>Private Room</p>
+                            <h3>$1200/month</h3>    
+                        </div>
+                        <div className="widgets">
+                            <p className='p1'>
+                                <div className="tiny1"></div>
+                            </p>
+                            <p className='p2'>
+                                <div className="tiny2"></div>
+                            </p>
+                            <p className='p3'>
+                                <div className="tiny3"></div>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="imagecontainer3">
+                        <div className="imgs3"></div>
+                        <div className="text">
+                            <h4>2578 Folsom street, san francisco, CA, 94110</h4>
+                            <p>Private Room</p>
+                            <h3>$1200/month</h3>
+                        </div>
+                        <div className="widgets">
+                            <p className='p1'>
+                                <div className="tiny1"></div>
+                            </p>
+                            <p className='p2'>
+                                <div className="tiny2"></div>
+                            </p>
+                            <p className='p3'>
+                                <div className="tiny3"></div>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="imagecontainer4">
+                        <div className="imgs4"></div>
+                        <div className="text">
+                            <h4>2578 Folsom street, san francisco, CA, 94110</h4>
+                            <p>Private Room</p>
+                            <h3>$1200/month</h3>
+                        </div>
+                        <div className="widgets">
+                            <p className='p1'>
+                                <div className="tiny1"></div>
+                            </p>
+                            <p className='p2'>
+                                <div className="tiny2"></div>
+                            </p>
+                            <p className='p3'>
+                                <div className="tiny3"></div>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="imagecontainer5">
+                        <div className="imgs5"></div>
+                        <div className="text">
+                            <h4>2578 Folsom street, san francisco, CA, 94110</h4>
+                            <p>Private Room</p>
+                            <h3>$1200/month</h3>
+                        </div>
+                        <div className="widgets">
+                            <p className='p1'>
+                                <div className="tiny1"></div>
+                            </p>
+                            <p className='p2'>
+                                <div className="tiny2"></div>
+                            </p>
+                            <p className='p3'>
+                                <div className="tiny3"></div>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="imagecontainer6">
+                        <div className="imgs6"></div>
+                        <div className="text">
+                            <h4>2578 Folsom street, san francisco, CA, 94110</h4>
+                            <p>Private Room</p>
+                            <h3>$1200/month</h3>
+                        </div>
+                        <div className="widgets">
+                            <p className='p1'>
+                                <div className="tiny1"></div>
+                            </p>
+                            <p className='p2'>
+                                <div className="tiny2"></div>
+                            </p>
+                            <p className='p3'>
+                                <div className="tiny3"></div>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </main>
+  )
+}
+
+export default First
